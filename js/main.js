3a3b45c7b1c933dc3a853239df02847b9cb0a8e0
Nu4ego <144213766+Nu4ego@users.noreply.github.com>
2024-01-25 03:32:39 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,199 @@
+// let a=1
+// switch (a){
+//     case 1:
+//         console.log(1)
+//         break;
+//     case 1:
+//         console.log(2)
+//         break;
+//     case 1:
+//         console.log(3)
+//         break;
+//     default:
+//         console.log('error')
+// }
+
+//a = 15 if a>5 else 0
+
+// let state = 9+15 < 3;
+// let a = state ? console.log('yes') : console.log('no')
+//     (2>6) ? console.log('yujikl') :
+//     (2*9==3) ? console.log('sdfikl') : console.log('false')
+
+// let i = 0;
+// while (i<6) console.log(i++);
+
+// do {
+//     console.log(i--);
+// } while (i>0);
+
+// for (let i=0; i<10; i++){
+//     console.log(i);
+// };
+
+//let array = [45, 12, 45, 78, 36]
+// let array = {
+//     'name' : 'www',
+//     'age' : 18,
+//     'email' : '312312'
+// }
+// for(let i in array){
+//     console.log(array[i]);
+// }
+
+
+//                                  ЗАДАЧИ:
+
+
+// alert('Ваше имя: ' + prompt('Ваше имя?' )); // Простая страница
+
+
+// let a = 1, b = 1; // Постфиксная и префиксная форма
+// let c = ++a; //2
+// let d = b++; //1
+// console.log(c)
+// console.log(d)
+
+
+//let a = 2; // Результат присваивания
+//let x = 1 + (a *= 2);
+//console.log(x) //5
+
+
+// let num1 = parseInt(prompt("Введите первое число:")); // Исправьте сложение
+// let num2 = parseInt(prompt("Введите второе число:"));
+
+// let sum = num1 + num2;
+
+// console.log("Сумма чисел: " + sum);
+
+
+//                                   Работа с переменными
+// 1.
+// var num = 3; //
+// alert(num);
+
+// 2.
+// var a = 10;
+// var b = 2;
+
+// var sum = a + b;
+// var difference = a - b;
+// var product = a * b;
+// var quotient = a / b;
+
+// console.log("Sum:", sum);
+// console.log("Difference:", difference);
+// console.log("Product:", product);
+// console.log("Quotient:", quotient);
+
+// 3.
+// var c = 15;
+// var d = 2;
+
+// var result = c + d;
+
+// console.log(result);
+
+// 4.
+// var a = 10;
+// var b = 2;
+// var c = 5;
+
+// var sum = a + b + c;
+
+// console.log("Sum:", sum);
+
+// 5.
+// var a = 17;
+// var b = 10;
+
+// var c = a - b;
+// var d = 7;
+
+// var result = c + d;
+
+// console.log(result);
+
+//                                   Работа со строками
+// 1.
+// var str = 'Привет, Мир!';
+// document.write(str);
+// 
+// 2.
+// var str1 = 'Привет, ';
+// var str2 = 'Мир!';
+
+// var phrase = str1 + str2;
+
+// console.log(phrase);
+
+// 3.
+// var name = "Your Name";
+// console.log("Привет, " + name + "!");
+
+// 4.
+// var age = 25; // Replace 25 with your actual age
+
+// console.log("Мне " + age + " лет!");
+
+//                                   Функции prompt
+// 1.
+// var name = prompt("Please enter your name:");
+// alert("Your name is " + name);
+
+// 2.
+// var number = prompt("Please enter a number:");
+// var square = number * number;
+
+// alert("The square of the number is: " + square);
+
+//                                  Обращение к символам строки
+// 1.
+// var str = 'abcde';
+
+// var charA = str[0];
+// var charC = str[2];
+// var charE = str[4];
+
+// console.log(charA);
+// console.log(charC);
+// console.log(charE);
+
+// 2.
+// let num = '12345';
+// let product = 1;
+
+// for (let i = 0; i < num.length; i++) {
+//   let digit = parseInt(num[i]);
+//   product *= digit;
+// }
+
+// console.log(product);
+
+//                                  Практика
+// 1.
+// let secondsInHour = 60 * 60;
+// let secondsInDay = 24 * 60 * 60;
+// let secondsInMonth = 30 * 24 * 60 * 60;
+
+// console.log("Количество секунд в часе: " + secondsInHour);
+// console.log("Количество секунд в сутках: " + secondsInDay);
+// console.log("Количество секунд в месяце: " + secondsInMonth);
+
+// 2.
+// let date = new Date();
+// let hour = date.getHours();
+// let minute = date.getMinutes();
+// let second = date.getSeconds();
+
+// let time = hour + ":" + minute + ":" + second;
+// console.log("Текущее время: " + time);
+
+// 3.
+
+
+
+
+
+
